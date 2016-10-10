c309188f9eb7a904b8736cbebb36817fb47e78c2
Frank Haverkamp <haver@linux.vnet.ibm.com>
2016-10-10 17:09:07 +0200

    Add locking to avoid double execution which can cause confusion
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- misc/basic_hardware_tests.sh
+++ misc/basic_hardware_tests.sh
@@ -25,6 +25,52 @@
 export PATH=`pwd`/tools:$PATH
 export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
 
+# lock dirs/files
+LOCKDIR="/tmp/genwqe_hardware_tests.lock"
+PIDFILE="${LOCKDIR}/PID"
+
+# exit codes and text
+ENO_SUCCESS=0;  ETXT[0]="ENO_SUCCESS"
+ENO_GENERAL=1;  ETXT[1]="ENO_GENERAL"
+ENO_LOCKFAIL=2; ETXT[2]="ENO_LOCKFAIL"
+ENO_RECVSIG=3;  ETXT[3]="ENO_RECVSIG"
+
+trap 'ECODE=$?; echo "Exit: ${ETXT[ECODE]}($ECODE)" >&2' 0
+echo -n "Locking: " >&2
+
+if mkdir "${LOCKDIR}" &>/dev/null; then
+    # Lock succeeded, install signal handlers before storing the PID
+    # just in case storing the PID fails.
+    trap 'ECODE=$?;
+          echo "Removing lock. Exit: ${ETXT[ECODE]}($ECODE)" >&2
+          rm -rf "${LOCKDIR}"' 0
+    echo "$$" >"${PIDFILE}" 
+    # The following handler will exit the script upon receiving these
+    # signals the trap on "0" (EXIT) from above will be triggered by
+    # this trap's "exit" command!
+    trap 'echo "Killed by a signal." >&2
+          exit ${ENO_RECVSIG}' 1 2 3 15
+else
+    # If cat isn't able to read the file, another instance is probably
+    # about to remove the lock -- exit, we're *still* locked.
+    # Lock failed, check if the other PID is alive.
+    OTHERPID="$(cat "${PIDFILE}")"
+    if [ $? != 0 ]; then
+        echo "Lock failed, PID ${OTHERPID} is active" >&2
+        exit ${ENO_LOCKFAIL}
+    fi
+    if ! kill -0 $OTHERPID &>/dev/null; then
+        # lock is stale, remove it and restart
+        echo "Removing stale lock of nonexistant PID ${OTHERPID}" >&2
+        rm -rf "${LOCKDIR}"
+        exit ${ENO_LOCKFAIL}
+    else
+        # Lock is valid and OTHERPID is active - exit, we're locked!
+        echo "Lock failed, PID ${OTHERPID} is active" >&2
+        exit ${ENO_LOCKFAIL}
+    fi
+fi
+
 # Checks
 if [ ! -f cantrbry.tar.gz ]; then
 	echo "We need test case data: cantrbry.tar.gz"
