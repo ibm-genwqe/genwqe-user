b8ba50e857c377ae4b5e5d5fea0e67f4d094fcc7
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-11-24 10:45:15 +0100

    GenWQE: Fix for deflateBound
    Signed-off-by: Xiao Hua Zeng <zengxhsh@ch.ibm.com>
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- lib/wrapper.h
+++ lib/wrapper.h
@@ -43,6 +43,12 @@
 			_a < _b ? _a : _b; })
 #endif
 
+#ifndef MAX
+#  define MAX(a,b)	({ __typeof__ (a) _a = (a); \
+			   __typeof__ (b) _b = (b); \
+			_a > _b ? _a : _b; })
+#endif
+
 #ifndef __unused
 #  define __unused __attribute__((unused))
 #endif
